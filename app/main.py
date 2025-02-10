6b1d7d5ff862298923ee80de2664ad69241a33dd
Nikola <djura97.nd@gmail.com>
2025-02-11 00:44:32 +0100

    Resolve conflicts with master

--- app/main.py
+++ app/main.py
@@ -6,11 +6,10 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, str]:
     return {"Hello": "EPAM PROJECT START"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None) -> dict[str, Union[str, int] | None]:
     return {"item_id": item_id, "q": q}
-
