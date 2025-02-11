634e4aa6cd0bb76af3fcf34ab28f413eca957d3b
Nikola <djura97.nd@gmail.com>
2025-02-11 22:16:31 +0100

    Updated main.py.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,9 +8,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "EPAM PROJECT START"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
