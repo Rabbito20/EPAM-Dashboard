f6078addc9bba373c3ec7979a1497f0e0b99c611
Nikola Djurovic <38770281+Rabbito20@users.noreply.github.com>
2025-02-23 20:34:48 +0100

    Add Dockerfile (#4)

---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+test_client = TestClient(app)
+
+
+def test_read_root():
+    response = test_client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "EPAM PROJECT START"}
+
+
+def test_read_item():
+    response = test_client.get("/items/1")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 1, "q": None}
