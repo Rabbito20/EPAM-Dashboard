1941bf4b5645c2dd28b78b9616c2e3339cb2df06
Nikola Djurovic <38770281+Rabbito20@users.noreply.github.com>
2025-02-23 20:34:48 +0100

    feat: add base logic

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,13 @@ from app.main import app
 test_client = TestClient(app)
 
 
-def test_read_root() -> None:
+def test_read_root():
     response = test_client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "EPAM PROJECT START"}
 
 
-def test_read_item() -> None:
+def test_read_item():
     response = test_client.get("/items/1")
     assert response.status_code == 200
-    assert response.json() == {"item_id": "1", "q": ""}
+    assert response.json() == {"item_id": 1, "q": None}
