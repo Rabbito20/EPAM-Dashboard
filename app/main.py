fcc872af06c95dd72bed306cf30aa9bda410d49b
Nikola <djura97.nd@gmail.com>
2025-03-06 18:23:39 +0100

    feat: removed commented line from main

--- app/main.py
+++ app/main.py
@@ -11,5 +11,3 @@ def read_root() -> dict[str, str]:
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = "") -> dict[str, str]:
     return {"item_id": f"{item_id}", "q": q}
-
-#   Ideja: Squash commits bez komande 'rebase'
