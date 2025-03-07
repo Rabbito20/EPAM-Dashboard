3ca00021cb24583ce66f9cc601849f0efb7450f2
Nikola <djura97.nd@gmail.com>
2025-03-07 18:13:30 +0100

    feat: add tests

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 import random
 
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 projects = []
+
+
+class ProjectCreateRequest(BaseModel):
+    project_name: str
+    project_author: str
+    description: str
 
 
 class Project:
@@ -25,18 +32,21 @@ class Project:
 
 
 class User:
-    username: str
-    user_id: int
-
-    def __init__(self):
-        self.username = None
+    def __init__(self, username):
+        self.username = username
         self.user_id = random.Random().getrandbits(8)
 
 
 #   Create project
 @app.post("/projects", status_code=200)
-def create_projects(project_name, project_author, description):
-    projects.append(Project(project_name, project_author, description))
+def create_projects(project: ProjectCreateRequest):
+    p = Project(
+        project_name=project.project_name,
+        project_author=project.project_author,
+        description=project.description
+    )
+    projects.append(p)
+    return {"message": "Project created", "project_id": p.project_id}
 
 
 #   Get all Projects
@@ -45,14 +55,14 @@ def get_all_projects():
     return {"project_id's": projects}
 
 
-#   Get specific project details
+#   Get project details
 @app.get("/project/{project_id}", status_code=200)
 def get_project_details(project_id: int):
     for _ in projects:
         if _.project_id == project_id:
             return _.get_project_details()
 
-    return HTTPException(status_code=404, detail="Project not found")
+    raise HTTPException(status_code=404, detail="Project not found")
 
 
 #   UPDATE project
@@ -64,7 +74,7 @@ def update_project(project_id: int):
 #   DELETE a project
 #   todo
 
-#   Test code
+#   Fill list for now, remove when DB gets implemented
 p = Project(project_name="Proj 5", project_author="Nikola", description="Desc")
 p1 = Project(project_name="Proj 6", project_author="Simon", description="Desc something about proj")
 p2 = Project(project_name="Proj 7", project_author="Denis", description="Desc desc")
