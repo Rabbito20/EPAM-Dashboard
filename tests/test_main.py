583b80bfe93c178bdd7a0aa2987f3e5b6e0c19b4
Nikola <djura97.nd@gmail.com>
2025-03-07 18:13:30 +0100

    feat: add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,38 @@
 from fastapi.testclient import TestClient
 
-from app.main import app
+from app.main import app, projects, Project
 
-test_client = TestClient(app)
+client = TestClient(app)
 
 
-def test_read_root():
-    response = test_client.get("/")
+def test_create_project():
+    response = client.post(
+        "/projects",
+        json={
+            "project_name": "Test Project",
+            "project_author": "Test Author",
+            "description": "Test Description"
+        }
+    )
     assert response.status_code == 200
-    assert response.json() == {"Hello": "EPAM PROJECT START"}
+    assert len(projects) > 0
 
 
-def test_read_item():
-    response = test_client.get("/items/1")
+def test_get_all_projects():
+    response = client.get("/projects")
+    assert response.status_code == 201
+    assert "project_id's" in response.json()
+
+
+def test_get_project_details():
+    test_project = Project("Sample Project", "Author", "Description")
+    projects.append(test_project)
+
+    response = client.get(f"/project/{test_project.project_id}")
     assert response.status_code == 200
-    assert response.json() == {"item_id": 1, "q": None}
+    assert response.json()["project_name"] == "Sample Project"
+
+
+def test_get_project_not_found():
+    response = client.get("/project/9999")  # non-existent ID
+    assert response.status_code == 404
